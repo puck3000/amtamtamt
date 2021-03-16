1363d3eb6b258338017e49293c99044be41fbbac
Daniel Lerch <daniel@daniellerch.ch>
2021-03-17 00:29:14 +0100

    adds fonts

--- components/Scratchpad.js
+++ components/Scratchpad.js
@@ -15,6 +15,30 @@ export default function Scratchpad() {
     return Math.floor(boardDimensions.height / 2 + 50)
   }
 
+  const getRandomX = () => {
+    const randomX =
+      Math.ceil(Math.random() * boardDimensions.width) *
+      (Math.round(Math.random()) ? 1 : -1)
+    return randomX
+  }
+
+  const fallingBenjamins = () => {
+    return (
+      <motion.span
+        initial={{
+          y: calculateFallingValues() * -1,
+          x: getRandomX(),
+        }}
+        animate={{ y: calculateFallingValues() }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
+        $
+      </motion.span>
+    )
+  }
+
   useEffect(() => {
     let timedBoardDimensions = setTimeout(() => {
       setBoardDimensions(constraintsRef.current.getBoundingClientRect())
@@ -22,15 +46,15 @@ export default function Scratchpad() {
     return () => {
       clearTimeout(timedBoardDimensions)
     }
-  })
+  }, [constraintsRef])
 
   return (
     <article
       ref={constraintsRef}
-      className='relative w-full h-full flex justify-center items-center overflow-hidden'
+      className='w-full h-full flex justify-center items-center'
     >
       {isPlaying ? (
-        <div>
+        <div className='relative w-full h-full flex justify-center items-center overflow-hidden'>
           <p className='absolute left-2 top-1'>$: {score}</p>
           <motion.div
             drag
@@ -53,6 +77,7 @@ export default function Scratchpad() {
               }}
             ></span>
           </motion.div>
+          {fallingBenjamins()}
         </div>
       ) : (
         <button onClick={() => setIsPlaying(true)}>Play</button>
