b36bfb58b4b6bab407be423385ffb698ca74aafe
Daniel Lerch <daniel@daniellerch.ch>
2021-03-16 14:05:23 +0100

    tuning grid

---
+++ pages/amt.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Amt() {
+  return (
+    <div className='md:h-screen md:flex md:flex-col md:justify-center md:items-center'>
+      <Head>
+        <title>AmtAmtAmt</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      {/* <header>header</header> */}
+      <main className='m-1 md:w-5/6'></main>
+      {/* <footer>footer</footer> */}
+    </div>
+  )
+}
