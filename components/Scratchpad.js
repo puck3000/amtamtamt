5de8c48aee84656d0783d411a6fc70b746cdfaa1
Daniel Lerch <daniel@daniellerch.ch>
2021-03-17 08:06:58 +0100

    snapshot

--- components/Scratchpad.js
+++ components/Scratchpad.js
@@ -1,15 +1,12 @@
-import { motion, useMotionValue } from 'framer-motion'
-import Image from 'next/image'
+import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
 
 export default function Scratchpad() {
-  const [boardDimensions, setBoardDimensions] = useState()
+  const [boardDimensions, setBoardDimensions] = useState(300)
   const [isPlaying, setIsPlaying] = useState(false)
-  const [benjamins, setBenjamins] = useState([])
-  const [score, setScore] = useState(0)
+  const [benjaminId, setBenjaminId] = useState(-100)
 
   const constraintsRef = useRef(null)
-  const dollarRef = useRef(null)
 
   const calculateFallingValues = () => {
     return Math.floor(boardDimensions.height / 2 + 50)
@@ -39,6 +36,11 @@ export default function Scratchpad() {
     )
   }
 
+  const playGame = () => {
+    setIsPlaying(true)
+    setBenjaminId(0)
+  }
+
   useEffect(() => {
     let timedBoardDimensions = setTimeout(() => {
       setBoardDimensions(constraintsRef.current.getBoundingClientRect())
@@ -48,6 +50,14 @@ export default function Scratchpad() {
     }
   }, [constraintsRef])
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setBenjaminId((benjaminId) => benjaminId + 1)
+    }, 2000)
+    return () => {
+      clearTimeout(timer)
+    }
+  })
   return (
     <article
       ref={constraintsRef}
@@ -55,14 +65,20 @@ export default function Scratchpad() {
     >
       {isPlaying ? (
         <div className='relative w-full h-full flex justify-center items-center overflow-hidden'>
-          <p className='absolute left-2 top-1'>$: {score}</p>
+          <p
+            className={`absolute text-4xl left-8 top-4 ${
+              benjaminId == 2 ? 'animate-ping' : ''
+            }`}
+          >
+            $: 0
+          </p>
           <motion.div
             drag
             dragConstraints={constraintsRef}
             dragElastic={0}
             whileHover={{ scale: 1.7 }}
             whileTap={{ scale: 2.5 }}
-            onDrag={(event, info) => console.log(info.point.x, info.point.y)}
+            // onDrag={(event, info) => console.log(info.point.x, info.point.y)}
             className='cursor-pointer'
           >
             <span
@@ -78,9 +94,31 @@ export default function Scratchpad() {
             ></span>
           </motion.div>
           {fallingBenjamins()}
+          {benjaminId > 7 ? (
+            benjaminId > 9 ? (
+              benjaminId > 12 ? (
+                <a
+                  href='#'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  class='absolute bottom-2 text-3xl text-red-900'
+                >
+                  Kultur Ermöglichen <span>&rarr;</span>
+                </a>
+              ) : (
+                <p class='absolute bottom-2 text-3xl text-red-900'>
+                  Kulturschaffende oft auch.
+                </p>
+              )
+            ) : (
+              <p class='absolute bottom-2 text-3xl text-red-900'>frustriert?</p>
+            )
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
-        <button onClick={() => setIsPlaying(true)}>Play</button>
+        <button onClick={() => playGame()}>Play</button>
       )}
     </article>
   )
