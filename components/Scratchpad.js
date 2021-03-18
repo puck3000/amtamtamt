de2f00951515e74cb3b2e5db44c7a78f5e806ecb
Daniel Lerch <daniel@daniellerch.ch>
2021-03-18 14:52:12 +0100

    game works!!!

--- components/Scratchpad.js
+++ components/Scratchpad.js
@@ -3,44 +3,39 @@ import { useEffect, useRef, useState } from 'react'
 
 export default function Scratchpad() {
   const [boardDimensions, setBoardDimensions] = useState(300)
-  const [benjamins, setBenjamins] = useState([])
   const [isPlaying, setIsPlaying] = useState(false)
+  const [counter, setCounter] = useState(0)
+  const [benjamins, setBenjamins] = useState([
+    { id: 1, falling: false },
+    { id: 2, falling: false },
+    { id: 3, falling: false },
+    { id: 4, falling: false },
+    { id: 5, falling: false },
+    { id: 6, falling: false },
+    { id: 7, falling: false },
+    { id: 8, falling: false },
+    { id: 9, falling: false },
+    { id: 10, falling: false },
+  ])
+  const [fallingBenjamin, setFallingBenjamin] = useState(11)
 
   const constraintsRef = useRef(null)
 
-  const calculateFallingValues = () => {
-    return Math.floor(boardDimensions.height / 2 + 50)
-  }
-
-  const getRandomX = () => {
-    const randomX =
-      Math.ceil(Math.random() * boardDimensions.width) *
-      (Math.round(Math.random()) ? 1 : -1)
-    return randomX
+  function randomTime() {
+    return Math.floor(Math.random() * 1000 + 1000)
   }
 
   const fallingBenjamins = () => {
-    return (
-      <motion.span
-        initial={{
-          y: calculateFallingValues() * -1,
-          x: getRandomX(),
-        }}
-        animate={{ y: calculateFallingValues() }}
-        transition={{
-          duration: 1.5,
-        }}
-      >
-        $
-      </motion.span>
-    )
+    const randomBenjamin = Math.floor(Math.random() * 9 + 1)
+    setFallingBenjamin(randomBenjamin)
+    setCounter((counter) => counter + 1)
+    // execute indefinately with random interval
+    setTimeout(fallingBenjamins, randomTime())
   }
 
   const playGame = () => {
     setIsPlaying(true)
-    for (let i = 0; i < 10; i++) {
-      setBenjamins((benjamins) => [...benjamins, fallingBenjamins()])
-    }
+    fallingBenjamins()
   }
 
   useEffect(() => {
@@ -59,10 +54,37 @@ export default function Scratchpad() {
     >
       {isPlaying ? (
         <div className='relative w-full h-full flex justify-center items-center overflow-hidden'>
-          <p className={`absolute text-4xl left-8 top-4 `}>$: 0</p>
-          <ul class='absolute inset-0 grid grid-cols-10'>
+          <p className={`absolute text-4xl left-8 bottom-4 `}>$: 0</p>
+          <div className='text-4xl absolute text-red-900 top-4 z-10'>
+            {counter > 10 ? (
+              <a
+                href='https://www.amt-fuer-ermoeglichung.ch/support'
+                target='_blank'
+                rel='noopener'
+                className='cursor-pointer'
+              >
+                &rarr; Kultur ermöglichen
+              </a>
+            ) : counter > 7 ? (
+              <p>Kulturförderung leider oft auch nicht.</p>
+            ) : counter > 5 ? (
+              <p>funktioniert nicht?</p>
+            ) : (
+              ''
+            )}
+          </div>
+          <ul className='absolute inset-0 grid grid-cols-10'>
             {benjamins.map((dollar, i) => (
-              <li className={`mx-auto  transition  duration-1000 `}>$</li>
+              <li
+                key={i}
+                className={`mx-auto text-3xl transition-transform ease-in transform -mt-6 duration-1000 ${
+                  fallingBenjamin == dollar.id
+                    ? 'translate-y-full'
+                    : 'opacity-0'
+                } `}
+              >
+                $
+              </li>
             ))}
           </ul>
           <motion.div
@@ -86,40 +108,12 @@ export default function Scratchpad() {
               }}
             ></span>
           </motion.div>
-
-          {/* {benjaminId > 7 ? (
-            benjaminId > 9 ? (
-              benjaminId > 12 ? (
-                <a
-                  href='#'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  class='absolute bottom-2 text-3xl text-red-900'
-                >
-                  Kultur Ermöglichen <span>&rarr;</span>
-                </a>
-              ) : (
-                <p class='absolute bottom-2 text-3xl text-red-900'>
-                  Kulturschaffende oft auch.
-                </p>
-              )
-            ) : (
-              <p class='absolute bottom-2 text-3xl text-red-900'>frustriert?</p>
-            )
-          ) : (
-            <></>
-          )} */}
         </div>
       ) : (
-        <button onClick={() => playGame()}>Play</button>
+        <button className='text-4xl' onClick={() => playGame()}>
+          Play
+        </button>
       )}
-      {/* <style jsx>{`
-        @media screen and (max-width: 768px) {
-          article {
-            height: 50vh;
-          }
-        }
-      `}</style> */}
     </article>
   )
 }
