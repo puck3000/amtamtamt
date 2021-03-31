246181247e6370ea8d24d3177e68f8011468be05
Puck3000 <daniel.lerch@gmail.com>
2021-03-31 09:18:10 +0200

    adds lang attribute to html

---
+++ pages/_document.js
@@ -0,0 +1,11 @@
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='de'>
+        <Head></Head>
+        <Main />
+        <NextScript />
+      </Html>
+    )
+  }
+}
